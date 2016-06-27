696bc96a31851a98a6977a61d308d222bcdd990b
rapurasusann <rapurasusann@yahoo.co.jp>
2016-06-27 17:48:17 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,11 @@
-for(var i=1; i<100; i++){
+for(var i=1; i<=100; i++){
   var li = document.createElement('li');
-li.textContent =i+'回目';
+  if(var%3==0)
+  {
+    li.trxtContent = 'Fizz';
+  }
+  else
+li.textContent =i;
 document.getElementById('list').appendChild(li);
 }
 
