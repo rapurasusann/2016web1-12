809bb8cf1f08ecf47aa7aa2a00db7a74722d28f0
rapurasusann <rapurasusann@yahoo.co.jp>
2016-06-27 17:34:43 +0900

    Update default.js

--- is/default.js
+++ is/default.js
@@ -2,12 +2,3 @@ var li = document.createElement('li');
 li.textContent =’アイテム';
 document.getElementById('list').appendChild(li);
 
-document.getElementById('form').onsubmit = function(){
-  window.alert(document.getElementById('form').word.value);
-};
-
-for(var i　=1; i<100; i++){
-  ver li = document.createElement('li');
-  li.textContent = i+'回目';
-  document.getElementbyId('list').appendChild(li);
-}
