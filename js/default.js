0a4dfcb86b677016d92054b340f8ffd5b15a88ca
rapurasusann <rapurasusann@yahoo.co.jp>
2016-06-27 17:43:52 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,6 @@
-var li = document.createElement('li');
-li.textContent ='アイテム';
+for(ver i=1; i<100; i++9{
+  var li = document.createElement('li');
+li.textContent ='i+'回目';
 document.getElementById('list').appendChild(li);
+}
 
