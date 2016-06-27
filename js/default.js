9aaff6331626043ef6c36a165073f36b1e50f506
rapurasusann <rapurasusann@yahoo.co.jp>
2016-06-27 18:00:08 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,16 +1,17 @@
 for(var i=1; i<=100; i++){
   var li = document.createElement('li');
-  if(i%3==0)
+  
+  if(i%3==0&&i%5==0)
+  {
+    li.textContent = 'FizzBuzz';
+  }
+  else if(i%3==0)
   {
     li.textContent = 'Fizz';
   }
   else if(i%5==0)
   {
     li.textContent = 'Buzz';
-  }
-  else if(i%3==0&&i%5==0)
-  {
-    li.textContent = 'FizzBuzz';
   }
   else
   {
