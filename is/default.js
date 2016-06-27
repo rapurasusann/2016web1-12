61fe73f93a5d9652961cddec5a9ec6c88f0fe31d
rapurasusann <rapurasusann@yahoo.co.jp>
2016-06-27 17:16:21 +0900

    Update default.js

--- is/default.js
+++ is/default.js
@@ -1,3 +1,7 @@
 Evar li = document.createElement('li');
 li.textContent =’アイテム'+
 document.getElementById('list').appendChild(li);
+
+document.getElementById('form').onsubmit = function(){
+  window.alert('押されました！');
+};
