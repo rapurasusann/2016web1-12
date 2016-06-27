b03c5771808fabcf3ffae6537ce9e9d765c6d15f
rapurasusann <rapurasusann@yahoo.co.jp>
2016-06-27 17:17:02 +0900

    Update default.js

--- is/default.js
+++ is/default.js
@@ -1,5 +1,5 @@
 Evar li = document.createElement('li');
-li.textContent =’アイテム'+
+li.textContent =’アイテム';
 document.getElementById('list').appendChild(li);
 
 document.getElementById('form').onsubmit = function(){
